124f202075cfea66e9d7b025b7e505be85110462
Tom Coleman <colemanto@gmail.com>
2015-05-01 13:33:16 -0500

    Rebooted!

---
+++ app/scripts/main.js
@@ -0,0 +1,18 @@
+/*global FitchTest, $*/
+
+
+window.FitchTest = {
+    Models: {},
+    Collections: {},
+    Views: {},
+    Routers: {},
+    init: function () {
+        'use strict';
+        console.log('Hello from Backbone!');
+    }
+};
+
+$(document).ready(function () {
+    'use strict';
+    FitchTest.init();
+});
